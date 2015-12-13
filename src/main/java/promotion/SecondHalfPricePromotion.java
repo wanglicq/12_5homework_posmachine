89c8e3a02e7b6904d746878a6b072ccb5803ae43
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-13 22:33:46 +0800

    Change allSecondHalfPrice in SecondHalfPromotion from List to Set for performance.

--- src/main/java/promotion/SecondHalfPricePromotion.java
+++ src/main/java/promotion/SecondHalfPricePromotion.java
@@ -7,18 +7,25 @@ import inter.ShopData;
 import parser.SecondHalfPriceParser;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
 
 public class SecondHalfPricePromotion implements Promotion {
 
     private Collection<Item> allItems;
-    private List<SecondHalfPrice> allSecondHalfPrice;
+    private Set<String> allSecondHalfPrice;
 
     public SecondHalfPricePromotion(Collection<Item> allItems) {
         this.allItems = allItems;
 
         SecondHalfPriceParser secondHalfPriceParser = new SecondHalfPriceParser();
-        allSecondHalfPrice = secondHalfPriceParser.parse(ShopData.SECOND_HALF_PRICE);
+
+        allSecondHalfPrice = secondHalfPriceParser
+                                .parse(ShopData.SECOND_HALF_PRICE)
+                                .stream()
+                                .map(SecondHalfPrice::getBarcode)
+                                .collect(toSet());
     }
 
     @Override
@@ -32,12 +39,8 @@ public class SecondHalfPricePromotion implements Promotion {
     private double getSavePrice(double price, CartItem cartItem) {
         String barcode = cartItem.getBarcode();
         Integer saveQuantity = cartItem.getQuantity() / 2;
-        for (SecondHalfPrice secondHalfPrice : allSecondHalfPrice) {
-            if (secondHalfPrice.getBarcode().equals(barcode)) {
-                return price / 2 * saveQuantity;
-            }
-        }
-        return 0;
+
+        return allSecondHalfPrice.contains(barcode)? price / 2 * saveQuantity: 0;
     }
 
 
