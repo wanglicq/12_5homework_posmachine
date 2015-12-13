67eebd0698159eec0e34ca2e42cc73e12296f906
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-13 22:05:20 +0800

    Format in 'Main.java'.

--- src/main/java/inter/Main.java
+++ src/main/java/inter/Main.java
@@ -8,12 +8,14 @@ import parser.ShoppingCartParser;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
+
         ItemParser itemParser = new ItemParser();
         List<Item> allItems = itemParser.parse(ShopData.ITEMS_DATA);
+
         ShoppingCartParser shoppingCartParser = new ShoppingCartParser();
         List<CartItem> cartItems = shoppingCartParser.parse(ShopData.SHOPPING_CART_DATA);
-
 
         PosMachine posMachine = new PosMachine(allItems);
         double total = posMachine.calculate(cartItems);
